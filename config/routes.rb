1752877db8868ee4b94e32f071e5167e30d0f83c
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-02-18 02:05:11 -0200

    Alterado scopo de routes e controllers, incluindo api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :products
+        resources :categories
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
