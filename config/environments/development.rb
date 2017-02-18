7c77dadfd4626913faad0c35d91fe8be32ccb0f4
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-02-18 02:24:08 -0200

    Corrigido roteamento

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,5 +45,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_dispatch.tld_length = 0
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        resources :products
-        resources :categories
-      end
-    end
-  end
+  # constraints subdomain: 'api' do
+    # scope 'api' do
+        namespace :api do
+            scope module: 'v1' do
+                resources :products
+                resources :categories
+            end
+        end
+    # end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
