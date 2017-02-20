911e8419bc9a12172a4fb7d2d8b8049d85fa40d3
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-02-20 15:03:59 -0300

    Adicionado outros metodos no Cors

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ProfiteStudyApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
