752a58424eab5bd9bb26bf1c2904949718a76041
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-03-14 11:57:38 -0300

    Atualizado schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170218050349) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -32,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170218050349) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "category_id"
-    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +40,4 @@ ActiveRecord::Schema.define(version: 20170218050349) do
     t.string   "api_key"
   end
 
-  add_foreign_key "products", "categories"
 end
