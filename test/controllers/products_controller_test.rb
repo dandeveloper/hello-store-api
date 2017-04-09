cf21228f2a230af5c6a8a424ceeb8e8acd9a1a87
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-04-09 03:35:22 -0300

    Maior cobertura nos testes

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,8 +3,10 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    @product = products(:one)
+    @product_one = products(:one)
+    @product_two = products(:one)
     @token = generate_api_key
+    Category.create(name: "Categoria Teste", description: "Descrição da categoria teste")
   end
 
   test "should get index" do
@@ -12,27 +14,32 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create product" do
-  #   assert_difference('Product.count') do
-  #     post api_products_url, params: { product: { best_price: @product.best_price, description: @product.description, name: @product.name, price: @product.price, quantity: @product.quantity, category_id: @product.category_id, short_description: @product.short_description } }, as: :json, headers: { "Authorization" => "Token token=#{@token}" }
-  #   end
-  #
-  #   assert_response 201
-  # end
+  test "should create product" do
+    assert_difference('Product.count') do
+      post api_products_url, params: { product: { name: @product_one.name, description: @product_one.description, short_description: @product_one.short_description, price: @product_one.price, best_price: @product_one.best_price, quantity: @product_one.quantity, category_id: Category.first.id } }, as: :json, headers: { "Authorization" => "Token token=#{@token}" }
+    end
+  
+    assert_response 201
+  end
 
   test "should show product" do
-    get api_product_url(@product), as: :json, headers: { "Authorization" => "Token token=#{@token}" }
+    get api_product_url(@product_one), as: :json, headers: { "Authorization" => "Token token=#{@token}" }
     assert_response :success
   end
 
   # test "should update product" do
-  #   patch api_product_url(@product), params: { product: { best_price: @product.best_price, description: @product.description, name: @product.name, price: @product.price, quantity: @product.quantity, category_id: @product.category_id, short_description: @product.short_description } }, as: :json, headers: { "Authorization" => "Token token=#{@token}" }
+  #   patch api_products_url(@product_one), params: { product: { name: @product_two.name, description: @product_two.description, short_description: @product_two.short_description, price: @product_two.price, best_price: @product_two.best_price, quantity: @product_two.quantity, category_id: Category.first.id } }, as: :json, headers: { "Authorization" => "Token token=#{@token}" }
   #   assert_response 200
   # end
 
+  test "should update product" do
+    patch api_product_url(@product_one), params: { product: { name: @product_one.name, description: @product_one.description, short_description: @product_one.short_description, price: @product_one.price, best_price: @product_one.best_price, quantity: @product_one.quantity } }, as: :json, headers: { "Authorization" => "Token token=#{@token}" }
+    assert_response 200
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
-      delete api_product_url(@product), as: :json, headers: { "Authorization" => "Token token=#{@token}" }
+      delete api_product_url(@product_one), as: :json, headers: { "Authorization" => "Token token=#{@token}" }
     end
 
     assert_response 204
