79502ddf72170e648bb7dd8c5c6fcfcf26d6460f
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-04-09 23:09:01 -0300

    Criado migration image to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218050349) do
+ActiveRecord::Schema.define(version: 20170410005500) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170218050349) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "category_id"
+    t.string   "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
